171ea728cc0ba50088f6887d2971b747e827e6c4
harivinodn <harivinodn@gmail.com>
2021-09-16 15:06:19 +0530

    second commit

--- lookup.rb
+++ lookup.rb
@@ -20,29 +20,36 @@ dns_raw = File.readlines("zone")
 
 # ..
 # ..
-# FILL YOUR CODE HERE
 
-# Step 1. Filter  the comments (lines starting with #), and empty lines from the zone file.
-
-puts domain
+# Filter  the comments (lines starting with #), and empty lines from the zone file.
+def parse_dns(dns_dow_data)
+  dns_record_data = dns_dow_data.select { |line| line[0] != "#" && line[0] != "\n"}
+end
 
 
 # Step 2: Iterate over each line, split it into an array with 3 columns using the .split method,
 #         and build a hash which represents dns_records.
-# Note:   When splitting each line in the zone file, you might want to use the .strip method
-#         on each of the columns so empty spaces are removed.
 
+def resolve(dns_records_data, lookup_chain_data, domain_data)
+  dns_hash = {}
+  dns_records_data.each_with_index { |line, index|
+    line_split= line.split(", ")
+    line_split[2] = line_split[2].strip
 
+    dns_hash[line_split[1]] = {}
+    dns_hash[line_split[1]]. => line_split[2]
+  }
+  puts dns_hash
 
-# Step 3:
-
-# ..
-# ..
+  lookup_chain_data
+end
 
 # To complete the assignment, implement `parse_dns` and `resolve`.
 # Remember to implement them above this line since in Ruby
 # you can invoke a function only after it is defined.
+
 dns_records = parse_dns(dns_raw)
+puts dns_records
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
-puts lookup_chain.join(" => ")
+#puts lookup_chain.join(" => ")
