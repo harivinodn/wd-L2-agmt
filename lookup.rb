defee0eb55db3099ecd52f48ed230b988b9b7471
harivinodn <harivinodn@gmail.com>
2021-09-16 10:06:24 +0530

    first commit

---
+++ lookup.rb
@@ -0,0 +1,48 @@
+def get_command_line_argument
+  # ARGV is an array that Ruby defines for us,
+  # which contains all the arguments we passed to it
+  # when invoking the script from the command line.
+  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
+  if ARGV.empty?
+    puts "Usage: ruby lookup.rb <domain>"
+    exit
+  end
+  ARGV.first
+end
+
+# `domain` contains the domain name we have to look up.
+domain = get_command_line_argument
+
+# File.readlines reads a file and returns an
+# array of string, where each element is a line
+# https://www.rubydoc.info/stdlib/core/IO:readlines
+dns_raw = File.readlines("zone")
+
+# ..
+# ..
+# FILL YOUR CODE HERE
+
+# Step 1. Filter  the comments (lines starting with #), and empty lines from the zone file.
+
+puts domain
+
+
+# Step 2: Iterate over each line, split it into an array with 3 columns using the .split method,
+#         and build a hash which represents dns_records.
+# Note:   When splitting each line in the zone file, you might want to use the .strip method
+#         on each of the columns so empty spaces are removed.
+
+
+
+# Step 3:
+
+# ..
+# ..
+
+# To complete the assignment, implement `parse_dns` and `resolve`.
+# Remember to implement them above this line since in Ruby
+# you can invoke a function only after it is defined.
+dns_records = parse_dns(dns_raw)
+lookup_chain = [domain]
+lookup_chain = resolve(dns_records, lookup_chain, domain)
+puts lookup_chain.join(" => ")
