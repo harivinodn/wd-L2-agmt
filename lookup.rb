ad3bb39ec8937213870826344551d1eb1a2a5da7
harivinodn <harivinodn@gmail.com>
2021-09-23 14:56:56 +0530

    lookup table for domainname

--- lookup.rb
+++ lookup.rb
@@ -20,28 +20,45 @@ dns_raw = File.readlines("zone")
 
 # ..
 # ..
-
-# Filter  the comments (lines starting with #), and empty lines from the zone file.
+# Step 1: Filter  the comments (lines starting with #), and empty lines from the zone file.
 def parse_dns(dns_dow_data)
   dns_record_data = dns_dow_data.select { |line| line[0] != "#" && line[0] != "\n"}
 end
-
-
+#
 # Step 2: Iterate over each line, split it into an array with 3 columns using the .split method,
 #         and build a hash which represents dns_records.
 
-def resolve(dns_records_data, lookup_chain_data, domain_data)
-  dns_hash = {}
-  dns_records_data.each_with_index { |line, index|
+def split_and_strip(dns_records_data)
+  dns_records_data.each_with_index{ |line, index|
     line_split= line.split(", ")
-    line_split[2] = line_split[2].strip
-
-    dns_hash[line_split[1]] = {}
-    dns_hash[line_split[1]]. => line_split[2]
+    line_split[2] = line_split[2].strip # to remove newline
+    dns_records_data[index] = line_split
   }
-  puts dns_hash
+  dns_records_data
+end
 
-  lookup_chain_data
+def resolve(dns_records_data, lookup_chain_data, domain_data)
+  #dns_hash = {}
+  #dns_records_data.each { |line|
+  #  (dns_hash[line[0]] ||= []) << [line[1], line[2]]
+  #}
+  #puts dns_hash
+
+  current_domain = lookup_chain_data.last()
+  dns_records_data.each { |x|
+    if x[0]=="A" and x[1]==current_domain
+      lookup_chain_data.push(x[2])
+      return lookup_chain_data
+    end
+    if x[0]=="CNAME" and x[1]==current_domain
+      lookup_chain_data.push(x[2])
+      lookup_chain_data = resolve(dns_records_data, lookup_chain_data, domain_data)
+      return lookup_chain_data
+    end
+  }
+  err_message = "Error: record not found for #{lookup_chain_data[0]}"
+  lookup_chain_data.pop
+  lookup_chain_data.push(err_message)
 end
 
 # To complete the assignment, implement `parse_dns` and `resolve`.
@@ -49,7 +66,7 @@ end
 # you can invoke a function only after it is defined.
 
 dns_records = parse_dns(dns_raw)
-puts dns_records
+dns_records = split_and_strip(dns_records)
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
-#puts lookup_chain.join(" => ")
+puts lookup_chain.join(" => ")
