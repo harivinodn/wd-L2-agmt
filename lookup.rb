1f9a96f3f447265808474c6db6de306e1ec23b16
harivinodn <harivinodn@gmail.com>
2021-09-24 14:36:21 +0530

    v2 parse_dns hash implemented

--- lookup.rb
+++ lookup.rb
@@ -18,17 +18,15 @@ domain = get_command_line_argument
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
-# ..
-# ..
 # Step 1: Filter  the comments (lines starting with #), and empty lines from the zone file.
-def parse_dns(dns_dow_data)
-  dns_record_data = dns_dow_data.select { |line| line[0] != "#" && line[0] != "\n"}
-end
-#
-# Step 2: Iterate over each line, split it into an array with 3 columns using the .split method,
-#         and build a hash which represents dns_records.
+def preprocess(dns_row_data)
 
-def split_and_strip(dns_records_data)
+  #puts dns_row_data.class
+
+  dns_records_data = dns_row_data.select { |line| line[0] != "#" && line[0] != "\n"}
+  #puts dns_records_data
+  #puts dns_records_data.class
+
   dns_records_data.each_with_index{ |line, index|
     line_split= line.split(", ")
     line_split[2] = line_split[2].strip # to remove newline
@@ -37,6 +35,22 @@ def split_and_strip(dns_records_data)
   dns_records_data
 end
 
+def parse_dns(dns_records_data)
+
+  dns_hash = {}
+  dns_records_data.each { |line|
+    dns_hash = {line[1].to_sym => {type: line[0], target: line[2]}}
+  }
+  dns_hash
+  puts dns_hash
+end
+
+# Step 2: Iterate over each line, split it into an array with 3 columns using the .split method,
+#         and build a hash which represents dns_records.
+
+
+
+
 def resolve(dns_records_data, lookup_chain_data, domain_data)
   #dns_hash = {}
   #dns_records_data.each { |line|
@@ -44,29 +58,27 @@ def resolve(dns_records_data, lookup_chain_data, domain_data)
   #}
   #puts dns_hash
 
-  current_domain = lookup_chain_data.last()
-  dns_records_data.each { |x|
-    if x[0]=="A" and x[1]==current_domain
-      lookup_chain_data.push(x[2])
-      return lookup_chain_data
-    end
-    if x[0]=="CNAME" and x[1]==current_domain
-      lookup_chain_data.push(x[2])
-      lookup_chain_data = resolve(dns_records_data, lookup_chain_data, domain_data)
-      return lookup_chain_data
-    end
-  }
-  err_message = "Error: record not found for #{lookup_chain_data[0]}"
-  lookup_chain_data.pop
-  lookup_chain_data.push(err_message)
+  record = dns_records[domain]
+  if (!record)
+    lookup_chain << "Error: Record not found for "+domain
+    return
+  elsif record[:type] == "CNAME"
+    # ...
+  elsif record[:type] == "A"
+    #...
+  else
+    lookup_chain << "Invalid record type for "+domain
+    return
+  end
 end
 
 # To complete the assignment, implement `parse_dns` and `resolve`.
 # Remember to implement them above this line since in Ruby
 # you can invoke a function only after it is defined.
+dns_records = preprocess(dns_raw)
+puts dns_records
 
-dns_records = parse_dns(dns_raw)
-dns_records = split_and_strip(dns_records)
-lookup_chain = [domain]
-lookup_chain = resolve(dns_records, lookup_chain, domain)
-puts lookup_chain.join(" => ")
+dns_records = parse_dns(dns_records)
+#lookup_chain = [domain]
+#lookup_chain = resolve(dns_records, lookup_chain, domain)
+#puts lookup_chain.join(" => ")
